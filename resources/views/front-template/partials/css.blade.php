ef94f20cd14116b75660936d3285864b5b87b09c
Tony Castillo <tonycastillo@ebiz.com.py>
2019-06-14 15:23:31 -0400

    preparando el navbar

--- resources/views/front-template/partials/css.blade.php
+++ resources/views/front-template/partials/css.blade.php
@@ -1,3 +1,7 @@
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="/libs/bootstrap/css/bootstrap.min.css">
+{{--Goole fonts--}}
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900&display=swap" rel="stylesheet">
+
+{{--main css--}}
 <link rel="stylesheet" href="/css/main.css">
