a799dbae8e4962549af00d3c8e75df9c472d7d46
Tony Castillo <tonycastillo@ebiz.com.py>
2019-06-25 17:54:58 -0400

    intentando agregar wow.js

--- resources/views/front-template/partials/js.blade.php
+++ resources/views/front-template/partials/js.blade.php
@@ -4,4 +4,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="/libs/bootstrap/js/bootstrap.min.js"></script>
 <script src="/libs/OwlCarousel/owl.carousel.min.js"></script>
+<script src="/node_modules/wow.js/dist/wow.min.js"></script>
 <script src="/js/main.js"></script>
